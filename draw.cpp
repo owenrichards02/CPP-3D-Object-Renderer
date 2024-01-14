d630c942ab9465e8ac8894db4935db2566ef5729
Owen Richards <owen.p.richards@gmail.com>
2024-01-14 23:38:08 +0000

    separated draw functions

--- draw.cpp
+++ draw.cpp
@@ -1,6 +1,8 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
+void draw(SDL_Window* window, SDL_Renderer* renderer);
+
 int main(int argc, char** argv){
     
     SDL_Window* window;
@@ -16,11 +18,9 @@ int main(int argc, char** argv){
     {
         SDL_CreateWindowAndRenderer(1920, 1080, 0, &window, &renderer); 
     }
-
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
-    SDL_RenderPresent(renderer);
+    
+    draw(window, renderer);
+    
 
     while (active)
     {
@@ -37,4 +37,11 @@ int main(int argc, char** argv){
     SDL_Quit();
 
     return 0;
+}
+
+void draw(SDL_Window* window, SDL_Renderer* renderer){
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
 }

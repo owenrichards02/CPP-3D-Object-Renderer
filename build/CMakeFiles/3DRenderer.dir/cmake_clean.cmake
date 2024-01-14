f691be61d18e31ecc916286e03e3c3717419ab3d
Owen Richards <owen.p.richards@gmail.com>
2024-01-14 22:32:44 +0000

    basic cmake env setup

---
+++ build/CMakeFiles/3DRenderer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "3DRenderer.exe"
+  "3DRenderer.exe.manifest"
+  "3DRenderer.pdb"
+  "CMakeFiles/3DRenderer.dir/main.cpp.obj"
+  "CMakeFiles/3DRenderer.dir/main.cpp.obj.d"
+  "lib3DRenderer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3DRenderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Owenp/Desktop/CPP-3D-Object-Renderer
+# Build directory: C:/Users/Owenp/Desktop/CPP-3D-Object-Renderer/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

29250c0bced76ee7491a77006c60934b75ef8930
Owen Richards <owen.p.richards@gmail.com>
2024-01-14 23:29:06 +0000

    window and renderer setup, showing a coloured background

--- draw.cpp
+++ draw.cpp
@@ -1,7 +1,40 @@
 #include <SDL2/SDL.h>
+#include <iostream>
 
 int main(int argc, char** argv){
     
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+    bool active = true;
+    SDL_Event currentEvent;
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+        // returns -1 --> video error
+        std::cout << "SDL_Init error: " << SDL_GetError() << std::endl;
+    else
+    {
+        SDL_CreateWindowAndRenderer(1920, 1080, 0, &window, &renderer); 
+    }
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+
+    while (active)
+    {
+        SDL_WaitEvent(&currentEvent);
+ 
+        switch (currentEvent.type)
+        {
+            case SDL_QUIT:
+                active = false;
+                break;
+        }
+    }
+ 
+    SDL_Quit();
 
     return 0;
 }

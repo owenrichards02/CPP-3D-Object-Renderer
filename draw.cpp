aa70c69f21aedc9e8d18b8b004f394a4341cc399
Owen Richards <owen.p.richards@gmail.com>
2024-01-15 00:40:19 +0000

    drawing a 2D box from point arrays and edge indexes, getting used to SDL2

--- draw.cpp
+++ draw.cpp
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <iostream>
+#include <array>
 
 void draw(SDL_Window* window, SDL_Renderer* renderer);
 
@@ -40,8 +41,26 @@ int main(int argc, char** argv){
 }
 
 void draw(SDL_Window* window, SDL_Renderer* renderer){
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    //set black backg with green pen
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+
+    //draw box from just lines 
+    int vertexMatrix[4][2] = {{50,50}, {50,100}, {100,50}, {100,100}};
+    int edgesList[4][2] = {{0,1}, {0,2}, {1,3}, {2,3}};
+
+    for (int i = 0; i < sizeof(edgesList) / sizeof(int) / 2; i++){
+        //draws edge i
+        int x1 = vertexMatrix[edgesList[i][0]][0];
+        int y1 = vertexMatrix[edgesList[i][0]][1];
+        int x2 = vertexMatrix[edgesList[i][1]][0];
+        int y2 = vertexMatrix[edgesList[i][1]][1];
+        SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+
+        std::cout << "[DRAWING] (" << x1 << "," << x2 << "), (" << y1 << "," << y2 << ")'\n'";
+    }
 
     SDL_RenderPresent(renderer);
 }
+
